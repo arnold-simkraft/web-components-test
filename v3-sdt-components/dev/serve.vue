15107a8ade89e47c1fcdf4aca566c067c90c1d52
arnold-simkraft <arnold.dcunha@simkraft.com>
2022-07-18 15:44:40 +0530

    added sdt-components

---
+++ v3-sdt-components/dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { V3SdtComponentsSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: "ServeDev",
+  // components: {
+  //  V3SdtComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <SdtButton></SdtButton>
+  </div>
+</template>
---
+++ v3-sdt-components/src/lib-components/Button.vue
@@ -0,0 +1,57 @@
+<template>
+  <button class="btn">{{ textToDisplay }}</button>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  text?: string;
+}>();
+
+const textToDisplay = computed(() => {
+  return props.text ?? "Default Button Text";
+});
+</script>
+
+<!-- <script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    text: { type: String },
+  },
+});
+</script> -->
+
+<style scoped>
+.btn {
+  background-color: red;
+  border-width: 3px;
+  border-color: orange;
+  border-radius: 0;
+  border-style: solid;
+  color: white;
+  display: block;
+  margin-bottom: 0;
+  font-weight: normal;
+  text-align: center;
+  -ms-touch-action: manipulation;
+  touch-action: manipulation;
+  cursor: pointer;
+  white-space: nowrap;
+  padding: 6px 12px;
+  font-size: 18px;
+  line-height: 1.42857143;
+}
+
+.btn:active:hover,
+.btn:focus,
+.btn:hover {
+  background-color: #1b1b32;
+  border-width: 3px;
+  border-color: #000;
+  background-image: none;
+  color: #f5f6f7;
+}
+</style>
